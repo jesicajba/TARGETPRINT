8981ed559df0818c8f683c52f00abbc4214b53a5
Jesica <jesicajba@yahoo.com.ar>
2012-10-06 16:19:27 -0300

    cambios css y contacto

--- app/views/menu_superior.php
+++ app/views/menu_superior.php
@@ -1,98 +1,85 @@
 			<div class="grid_16">
 			  <ul class="nav main" id="menu">
 					<li id="first">
-						<a href="/">Equipos Multifuncion</a>
+						<a href="12/">Equipos Multifuncion</a>
 						<ul>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Multifuncion 1</a>
+								<a href="12/fluid/mootools/">Multifuncion 1</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Multifuncion 2</a>
+								<a href="12/fluid/jquery/">Multifuncion 2</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Multifuncion 3</a>
+								<a href="12/fluid/none/">Multifuncion 3</a>
 							</li>
 						</ul>
 					</li>
 					<li>
-						<a href="/">Impresoras</a>
+						<a href="16/">Impresoras</a>
 						<ul>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Impresoras 1</a>
+								<a href="16/fluid/mootools/">Impresoras 1</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Impresoras 2</a>
+								<a href="16/fluid/jquery/">Impresoras 2</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Impresoras 3</a>
+								<a href="16/fluid/none/">Impresoras 3</a>
 							</li>
 						</ul>
 					</li>
 					<li>
-						<a href="/">Insumos</a>
+						<a href="12/fixed/">Insumos</a>
 						<ul>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Insumos 1</a>
+								<a href="12/fixed/mootools/">Insumos 1</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Insumos 2</a>
+								<a href="12/fixed/jquery/">Insumos 2</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Insumos 3</a>
+								<a href="12/fixed/none/">Insumos 3</a>
 							</li>
 						</ul>
 					</li>
 					<li>
-						<a href="/">Insumos Alternativos</a>
+						<a href="16/fixed/">Insumos Alternativos</a>
 						<ul>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Alternativos 1</a>
+								<a href="16/fixed/mootools/">Alternativos 1</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Alternativos 2</a>
+								<a href="16/fixed/jquery/">Alternativos 2</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Alternativos 3</a>
+								<a href="16/fixed/none/">Alternativos 3</a>
 							</li>
 						</ul>
 					</li>
 					<li>
-						<a href="/">Telefonía</a>
+						<a href="http://symphony-cms.com/discuss/thread/231/">Telefonía</a>
 						<ul>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Telefonia 1</a>
+								<a href="http://github.com/bauhouse/fluid960gs/tree/">Telefonia 1</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Telefonia 2</a>
+								<a href="http://github.com/bauhouse/sym-fluid960gs/tree/">Telefonia 2</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Telefonia 3</a>
+								<a href="http://symphony-cms.com/discuss/thread/231/">Telefonia 3</a>
 							</li>
 						</ul>
 					</li>
                     <li>
-						<a href="/">Accesorios</a>
+						<a href="http://symphony-cms.com/discuss/thread/231/">Contacto</a>
 						<ul>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Accesorios 1</a>
+								<a href="http://github.com/bauhouse/fluid960gs/tree/">Consultas</a>
 							</li>
 							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Accesorios 2</a>
-							</li>
-							<li>
-								<a href="<?php echo site_url(); ?>/catalogo/">Accesorios 3</a>
-							</li>
+							<a href="http://github.com/bauhouse/sym-fluid960gs/tree/">Consultas al gremio</a></li>
 						</ul>
 					</li>
-					<?php if ($this->ion_auth->logged_in() && $this->ion_auth->is_admin()) : ?>
-						<li><a href="<?php echo site_url(); ?>/admin/">Administracion</a></li>
-					<?php endif; ?>
-					<?php if ($this->ion_auth->logged_in()) : ?>
-						<li><a href="<?php echo site_url(); ?>/auth/logout/">Logout</a></li>
-					<?php  else : ?>
-						<li><a href="<?php echo site_url(); ?>/auth/login/">Login</a></li>
-						<li><a href="<?php echo site_url(); ?>/auth/crear_usuario/">Registro</a></li>
-					<?php endif; ?>
 				</ul>
 			</div>
 			<div class="clear"></div>
